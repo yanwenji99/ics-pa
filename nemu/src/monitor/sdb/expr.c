8cb35c2f014cd8c90c5e52915ecf2e83f357f933
tracer-ics2025 <tracer@njuics.org>
2025-11-27 15:13:00 +0800

    >  compile NEMU 231220000 张三 Linux yan-virtual-machine 6.8.0-87-generic #88~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Oct 14 14:03:14 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:13:00 up 58 min,  1 user,  load average: 0.11, 0.12, 0.08

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -221,9 +221,10 @@ static word_t eval(Token *tokens,int p,int q) {
       case '-': return val1 - val2;
       case '*': return val1 * val2;
       case '/': return val1 / val2;
-      default: assert(0);
+      default: 
+        Assert(0, "Unknown operator"); 
+        return 0;
     }
-    return 0;
   }
 }
 

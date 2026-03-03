85849b1a900906b1eda7a960d502227b416175f0
tracer-ics2025 <tracer@njuics.org>
2026-03-03 21:04:15 +0800

    >  compile NEMU 231220000 张三 Linux yan-virtual-machine 6.8.0-101-generic #101~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Feb 11 13:19:54 UTC  x86_64 x86_64 x86_64 GNU/Linux  21:04:15 up 26 min,  0 users,  load average: 0.34, 0.14, 0.05

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -51,14 +51,22 @@ void init_mem() {
 }
 
 word_t paddr_read(paddr_t addr, int len) {
-  if (likely(in_pmem(addr))) return pmem_read(addr, len);
+  if (likely(in_pmem(addr))) {
+    word_t data = pmem_read(addr, len);
+    IFDEF(CONFIG_MTRACE, log_write("mtrace: pc = " FMT_WORD ", addr = " FMT_PADDR ", len = %d, type = R, data = " FMT_WORD "\n", cpu.pc, addr, len, data));
+    return data;
+  }
   IFDEF(CONFIG_DEVICE, return mmio_read(addr, len));
   out_of_bound(addr);
   return 0;
 }
 
 void paddr_write(paddr_t addr, int len, word_t data) {
-  if (likely(in_pmem(addr))) { pmem_write(addr, len, data); return; }
+  if (likely(in_pmem(addr))) {
+    IFDEF(CONFIG_MTRACE, log_write("mtrace: pc = " FMT_WORD ", addr = " FMT_PADDR ", len = %d, type = W, data = " FMT_WORD "\n", cpu.pc, addr, len, data));
+    pmem_write(addr, len, data);
+    return;
+  }
   IFDEF(CONFIG_DEVICE, mmio_write(addr, len, data); return);
   out_of_bound(addr);
 }

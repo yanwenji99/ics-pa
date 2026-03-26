c16b294d2c04e4780bfd0d87f7bac48bafb5537e
tracer-ics2025 <tracer@njuics.org>
2026-03-26 22:09:56 +0800

    >  compile NEMU 231220000 张三 Linux yan-virtual-machine 6.8.0-106-generic #106~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar  6 08:44:59 UTC  x86_64 x86_64 x86_64 GNU/Linux  22:09:56 up 26 min,  0 users,  load average: 0.08, 0.21, 0.20

--- nemu/src/isa/riscv32/difftest/dut.c
+++ nemu/src/isa/riscv32/difftest/dut.c
@@ -18,7 +18,13 @@
 #include "../local-include/reg.h"
 
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
-  return false;
+  for (int i = 0; i < MUXDEF(CONFIG_RVE, 16, 32); i ++) {
+    if (!difftest_check_reg(reg_name(i), pc, ref_r->gpr[i], cpu.gpr[i])) {
+      return false;
+    }
+  }
+
+  return difftest_check_reg("pc", pc, ref_r->pc, cpu.pc);
 }
 
 void isa_difftest_attach() {

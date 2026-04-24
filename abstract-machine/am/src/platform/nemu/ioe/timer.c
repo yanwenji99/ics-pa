d751263d8fa48bb108cac88eab46f6e35f8b23d0
tracer-ics2025 <tracer@njuics.org>
2026-04-24 23:28:00 +0800

    >  compile NEMU 231220000 张三 Linux yan-virtual-machine 6.8.0-107-generic #107~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Mar 18 23:40:43 UTC  x86_64 x86_64 x86_64 GNU/Linux  23:28:00 up  9:48,  1 user,  load average: 0.04, 0.39, 0.33

--- abstract-machine/am/src/platform/nemu/ioe/timer.c
+++ abstract-machine/am/src/platform/nemu/ioe/timer.c
@@ -7,7 +7,6 @@ void __am_timer_init()
 
 void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime)
 {
-  // NEMU updates both halves when reading RTC_ADDR + 4; read hi first to latch.
   uint32_t hi = inl(RTC_ADDR + 4);
   uint32_t lo = inl(RTC_ADDR + 0);
   uptime->us = ((uint64_t)hi << 32) | lo;

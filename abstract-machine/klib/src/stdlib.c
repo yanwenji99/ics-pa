c0c53b070947fffd26efbb739c29721ac20157d3
tracer-ics2025 <tracer@njuics.org>
2026-04-25 16:05:26 +0800

    >  compile NEMU 231220000 张三 Linux yan-virtual-machine 6.8.0-110-generic #110~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 27 12:43:08 UTC  x86_64 x86_64 x86_64 GNU/Linux  16:05:26 up  2:55,  0 users,  load average: 0.00, 0.11, 0.13

--- abstract-machine/klib/src/stdlib.c
+++ abstract-machine/klib/src/stdlib.c
@@ -5,41 +5,96 @@
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 static unsigned long int next = 1;
 
-int rand(void) {
+int rand(void)
+{
   // RAND_MAX assumed to be 32767
   next = next * 1103515245 + 12345;
-  return (unsigned int)(next/65536) % 32768;
+  return (unsigned int)(next / 65536) % 32768;
 }
 
-void srand(unsigned int seed) {
+void srand(unsigned int seed)
+{
   next = seed;
 }
 
-int abs(int x) {
+int abs(int x)
+{
   return (x < 0 ? -x : x);
 }
 
-int atoi(const char* nptr) {
+int atoi(const char *nptr)
+{
   int x = 0;
-  while (*nptr == ' ') { nptr ++; }
-  while (*nptr >= '0' && *nptr <= '9') {
+  while (*nptr == ' ')
+  {
+    nptr++;
+  }
+  while (*nptr >= '0' && *nptr <= '9')
+  {
     x = x * 10 + *nptr - '0';
-    nptr ++;
+    nptr++;
   }
   return x;
 }
 
-void *malloc(size_t size) {
+// 全局堆顶指针，0 表示尚未初始化
+static uintptr_t klib_brk = 0;
+
+void *malloc(size_t size)
+{
   // On native, malloc() will be called during initializaion of C runtime.
   // Therefore do not call panic() here, else it will yield a dead recursion:
   //   panic() -> putchar() -> (glibc) -> malloc() -> panic()
+  if (size == 0)
+  {
+    return NULL;
+  }
+
+  if (heap.start == NULL || heap.end == NULL)
+  {
+    return NULL;
+  }
+
+  const uintptr_t align = sizeof(uintptr_t);
+  const uintptr_t heap_end = (uintptr_t)heap.end;
+
+  if (klib_brk == 0)
+  {
+    klib_brk = ROUNDUP(heap.start, align);
+  }
+
+  uintptr_t p = ROUNDUP(klib_brk, align);
+  if (p > heap_end)
+  {
 #if !(defined(__ISA_NATIVE__) && defined(__NATIVE_USE_KLIB__))
-  panic("Not implemented");
+    panic("Out of memory");
 #endif
-  return NULL;
+    return NULL;
+  }
+
+  if ((uintptr_t)size > heap_end - p)
+  {
+#if !(defined(__ISA_NATIVE__) && defined(__NATIVE_USE_KLIB__))
+    panic("Out of memory");
+#endif
+    return NULL;
+  }
+
+  uintptr_t next = ROUNDUP(p + size, align);
+  if (next < p || next > heap_end)
+  {
+#if !(defined(__ISA_NATIVE__) && defined(__NATIVE_USE_KLIB__))
+    panic("Out of memory");
+#endif
+    return NULL;
+  }
+
+  klib_brk = next;
+  return (void *)p;
 }
 
-void free(void *ptr) {
+void free(void *ptr)
+{
 }
 
 #endif
